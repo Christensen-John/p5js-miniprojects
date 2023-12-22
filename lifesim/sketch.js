57daec28a7082bf2008c7f5a27dffe2fbfb23e86
Christensen-John <121368802+Christensen-John@users.noreply.github.com>
2023-12-22 17:52:12 +0100

    Create Object Oriented structure and begin cell and grid objects

--- lifesim/sketch.js
+++ lifesim/sketch.js
@@ -1,9 +1,9 @@
-let worldSize = 800;
-let cellSize = 8;
-let cells;
-let firstRun = 1;
-let cellColor = 20;
-let backgroundColor = 220;
+let __WORLD_SIZE = 800;
+let __CELL_SIZE = 8;
+let current_life_cycle;
+let next_life_cycle;
+let cell_color = 20;
+let background_color = 220;
 
 //P5 JS BASE FUNCTIONS
 function setup() {
@@ -16,7 +16,7 @@ function setup() {
 	simBtn.mousePressed(controlSim);
 
 	// Set up the data array to hold life tracking data
-	createPetriDish();
+	createCellGrid();
 
 	//Prevent looping until the simulation is started.
 	noLoop();
@@ -27,32 +27,16 @@ function draw() {
 }
 
 //FUNCTIONS FOR MANAGING THE MODEL
-function createPetriDish() {
-	//Create the base array of the grid of values
-	cells = [];
-
-	// Start loop to create each row
-	for (let i = 0; i < worldSize; i += cellSize) {
-		//create array to hold each column for a given row
-		let row = [];
-
-		//Loot through the columns
-		for (let j = 0; j < worldSize; j += cellSize) {
-			//Add the base value for the petri dish, false represents dead, all cells start as dead
-			row.push(false);
-		}
-
-		//The completed set of columns is added the the row array
-		cells.push(row);
-	}
-}
+function createCellGrid(size) {}
 
 function updateCell(x, y) {
 	//Get the alive status of the current clicked cell
-	let status = cells[x][y];
+	//Y and X are switched or the model array does not match the same grid direction as the view
+	let status = currentLifeCycle[y][x];
 
 	//Toggle the living status of the cell
-	cells[x][y] = !status;
+	//Y and X are switched or the model array does not match the same grid direction as the view
+	currentLifeCycle[y][x] = !status;
 
 	//Hide a living cell (kill it) or draw a new cell based on the previous status
 	if (status) {
@@ -61,6 +45,8 @@ function updateCell(x, y) {
 		drawNewCell(x, y);
 	}
 }
+
+function createNewLifeCycle() {}
 
 //FUNCTIONS FOR MANAGING USER CONTROL
 function mousePressed(event) {
@@ -73,7 +59,14 @@ function mousePressed(event) {
 }
 
 //Handles user pressing the start button
-function controlSim(event) {}
+function controlSim(event) {
+	if (isLooping()) {
+		noLoop();
+	} else {
+		loop();
+		draw();
+	}
+}
 
 //FUNCTIONS FOR MANAGING THE VIEW
 //draws a new alive cell at the current selected grid location
@@ -96,7 +89,7 @@ function drawAllCells() {
 	stroke(0);
 	for (let i = 0; i < cells.length; i++) {
 		for (let j = 0; j < cells[i].length; j++) {
-			if (cells[i][j]) {
+			if (currentLifeCycle[i][j]) {
 				square(i * cellSize, j * cellSize, cellSize, 2);
 			}
 		}
@@ -123,3 +116,85 @@ function drawAllCells() {
  * 3. For each cell, look at surrounding cells, calculate life state
  * 4. redraw grid with updated status
  */
+
+/**
+ * TODO:
+ * 1. Create clearAll button
+ */
+
+class life_grid {
+	//Instance property where the cells will be kept
+	life_grid = [];
+	grid_size = 0;
+
+	constructor(size) {
+		this.grid_size = size;
+		this.life_grid = createGrid(grid_size);
+	}
+
+	//Creates and returns a 2d array of the size of the life grid. Every value in the array is a dead cell (false)
+	createGrid(size) {
+		//Create the array to hold the rows of cells
+		let grid = [];
+		//loop through the number of rows
+		for (let i = 0; i < size; i++) {
+			//Create an array to hold the array of cells
+			let row = [];
+			//Loop though the columns of cells
+			for (let j = 0; j < size; j++) {
+				//create a new cell for each location
+				let cell = new life_cell(i, j, false);
+				//Add the cell to the row
+				row.push(cell);
+			}
+			//Add the row to the base grid array
+			grid.push(row);
+		}
+		//return the created grid of cells
+		return grid;
+	}
+
+	//Toggles the living status of the cell at location cellX, cellY. Returns the new living value
+	toggleLifeStatusOfCell(cellX, cellY) {
+		return this.life_grid[cellX][cellY].changeLifeStatus();
+	}
+
+	//Returns an array of life_cells representingi a row of the grid;
+	getGridRow(row) {
+		return this.life_grid[row];
+	}
+
+	//Generates a new grid of cells based on the previous generation
+	getNextGeneration() {
+		let nextGrid = createGrid(this.grid_size);
+	}
+}
+
+class life_cell {
+	x_location = 0;
+	y_location = 0;
+	living = false;
+
+	//Creates a new life_cell that has coordinates in a life_grid. Has a life status that defaults to false
+	constructor(x, y, startAlive = false) {
+		this.x_location = x;
+		this.y_location = y;
+		this.living = startAlive;
+	}
+
+	//Returns the life status of the cell
+	get isAlive() {
+		return this.living;
+	}
+
+	//Returns the location of the cell in the life_grid
+	get location() {
+		return [this.x_location, this.y_location];
+	}
+
+	//Toggles the life status of the cell. Returns the new life value
+	changeLifeStatus() {
+		this.living = !this.living;
+		return this.living;
+	}
+}
