6028eea2c578210c853f018ece51972458732dff
JohnnyIdahoan <121368802+Christensen-John@users.noreply.github.com>
2024-01-25 14:50:51 -0600

    Create basic style for webpage

--- generative/tube-grid/grid-cell.js
+++ generative/tube-grid/grid-cell.js
@@ -1,11 +1,12 @@
-const worldSize = 1000;
-const lineColor = 255;
-const cellsInGrid = 50;
-const randomLimit = 50;
-const lineStrokeWeight = 10;
+let worldSize = 1000;
+let lineColor = 255;
+let cellsInGrid = 50;
+let randomLimit = 50;
+let lineStrokeWeight = 10;
 
 function setup() {
-  createCanvas(1000, 1000);
+  const cnv = createCanvas(750, 750);
+  cnv.parent("canvas-container");
   background(200);
   noLoop();
 }
@@ -14,3 +15,18 @@ function draw() {
   let firstGrid = AngledGrid(worldSize, cellsInGrid, lineColor);
   firstGrid.drawGrid();
 }
+
+// function ProgramSettings() {
+
+//   const setWorldSize = (wrldSize) => (worldSize = wrldSize);
+//   const setLineColor = (newColor) => (lineColor = newColor);
+//   const setNumCellsInGrid = (newNumOfCells) => (cellsInGrid = newNumOfCells);
+//   const setRandomLimit = (newLimit) => (randomLimit = newLimit);
+//   const setStrokeWeight = (newStrokeWt) => (lineStrokeWeight = newStrokeWt);
+
+//   const getWorldSize() => worldSize;
+//   const getLineColor() => lineColor;
+//   const getCellsInGrid() => cellsInGrid;
+//   const getRandomLimit() => worldSize;
+//   const getWorldSize() => worldSize;
+// }
